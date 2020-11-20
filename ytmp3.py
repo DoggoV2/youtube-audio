cf3be36c66baf9a9f7b8d9fb8414746c47d6f2b4
DoggoV2 <72258939+DoggoV2@users.noreply.github.com>
2020-11-20 20:38:25 +0200

    Delays

--- ytmp3.py
+++ ytmp3.py
@@ -1,20 +1,20 @@
 from os import system
 import json
 from youtubesearchpython import SearchVideos
+import time
 
-
-
+time.sleep(3)
 se = input('Search: ')
 search = SearchVideos(se, offset = 1, mode = "json", max_results = 1)
 
-
+time.sleep(1)
 response = json.loads(search.result())
 
 link = response['search_result'][0]['link']
 
 
 print(response['search_result'][0]['title'])
-
+time.sleep(15)
 system('youtube-dl --force-ipv4 --extract-audio --audio-format mp3 '+link)
 
 
